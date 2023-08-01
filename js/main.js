3ecc8eec6b940f54df798992c33f132532216d95
SANISETTY HEMA SAGAR <93524527+HEMASAGAR07@users.noreply.github.com>
2023-08-02 00:08:24 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+const swup = new Swup();
+update();
+swup.on('contentReplaced', update);
+var temp = 0;
+
+function update() {
+    if (window.location.href == 'https://hemasagar07.github.io/index.html') {
+        document.getElementById('mm0').classList.add('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+
+
+
+        temp = 0;
+    }
+    if (window.location.href == 'https://hemasagar07.github.io/aboutMe.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.add('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+
+        temp = 1;
+    }
+    if (window.location.href == 'https://hemasagar07.github.io/acadamics.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.add('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.remove('active');
+
+        temp = 2;
+    }
+    if (window.location.href == 'https://hemasagar07.github.io/skills.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.add('active');
+        document.getElementById('mm4').classList.remove('active');
+
+        temp = 3;
+    }
+    if (window.location.href == 'https://hemasagar07.github.io/publications.html') {
+        document.getElementById('mm0').classList.remove('active');
+        document.getElementById('mm1').classList.remove('active');
+        document.getElementById('mm2').classList.remove('active');
+        document.getElementById('mm3').classList.remove('active');
+        document.getElementById('mm4').classList.add('active');
+        
+        temp = 4;
+    }
+
+    }
+    if (temp == 0) {
+        document.getElementById('m1').innerHTML = '<a href="aboutMe.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="publications.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 1) {
+        document.getElementById('m1').innerHTML = '<a href="acadamics.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="index.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 2) {
+        document.getElementById('m1').innerHTML = '<a href="skills.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="aboutMe.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 3) {
+        document.getElementById('m1').innerHTML = '<a href="publications.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="acadamics.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+    if (temp == 4) {
+        document.getElementById('m1').innerHTML = '<a href="index.html"><i class="fas fa-chevron-right"></i></i></a>';
+        document.getElementById('m2').innerHTML = '<a href="skills.html"><i class="fas fa-chevron-left"></i></i></i></a>';
+    }
+
+}
